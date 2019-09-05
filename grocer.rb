a74cf92a1e4ab9a574315dc838b642a6e41aa420
chrstphgrnr <christophgruener@me.com>
2019-09-05 14:07:59 -0600

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,8 +1,24 @@
+require 'pry'
 def consolidate_cart(cart)
+  # binding.pry
+  new_cart = {}
+  cart.each do |item|
+    if new_cart[item.keys[0]]
+      new_cart[item.keys[0]][:count] += 1
+    else 
+      new_cart[item.keys[0]] = {
+        :count => 1, 
+        :price => item.values[0][:price], 
+        :clearance => item.values[0][:clearance]
+      }
+    end
+  end
+  new_cart
   # code here
 end
 
 def apply_coupons(cart, coupons)
+  
   # code here
 end
 
